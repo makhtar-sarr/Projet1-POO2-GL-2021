c165bcdc14ed73f71c223236150d3a53985e0060
makhtar-sarr <makhtar.sarr@univ-thies.sn>
2021-07-30 17:12:35 +0000

    my commit

--- genieCivilOuvrage2D.py
+++ genieCivilOuvrage2D.py
@@ -6,6 +6,9 @@ Auteurs: Fatou DIOUF, <votre mail ici>
 """
 
 from turtle import*
+from math import*
+
+figure = Turtle()
 
 # Objectifs:
 # Methode:
@@ -15,3 +18,52 @@ from turtle import*
 # Sorties:
 # Resultats:
 # Hypotheses:
+def cercle(rayon, couleur = "black"):
+    figure.color(couleur)
+    figure.circle(rayon)
+
+# Objectifs:
+# Methode:
+# Besoins:
+# Connus:
+# Entrees:
+# Sorties:
+# Resultats:
+# Hypotheses:
+def demiCercle(rayon, couleur = "black"):
+    figure.color(couleur)
+    figure.circle(rayon, 180)
+
+# Objectifs:
+# Methode:
+# Besoins:
+# Connus:
+# Entrees:
+# Sorties:
+# Resultats:
+# Hypotheses:
+def carre(cote, couleur = "black", ramplis = "white"):
+    figure.color(couleur, ramplis)
+    for i in range(4):
+        figure.forward(cote)
+        figure.left(90)
+
+# Objectifs:
+# Methode:
+# Besoins:
+# Connus:
+# Entrees:
+# Sorties:
+# Resultats:
+# Hypotheses:
+def triangle(coteA, coteB, angleC):
+    coteC = sqrt((coteA**2) + (coteB**2) - (2*coteA*coteB*cos(angleC)))
+    figure.forward(coteA)
+    figure.left(180 - angleC)
+    figure.forward(coteB)
+    angleB = degrees(acos(((coteA**2) + (coteB**2) - (coteC**2))/(2*coteA*coteB)))
+    print(angleB)
+    figure.left(180 - angleB)
+    figure.forward(coteC)
+
+triangle(50, 50, 30)
