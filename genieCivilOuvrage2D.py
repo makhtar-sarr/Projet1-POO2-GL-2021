58e51f630281975a2d7c2ee1906ae3105db77931
makhtar-sarr <makhtar.sarr@univ-thies.sn>
2021-07-30 09:16:17 +0000

    my commit

---
+++ genieCivilOuvrage2D.py
@@ -0,0 +1,17 @@
+"""Cet module contient des fonction pour dessiner
+des figure geometrique avec la bibiotheque Turtle.
+
+Auteurs: Fatou DIOUF, <votre mail ici>
+         Makhtar SARR, makhtar.sarr@univ-thies.sn.
+"""
+
+from turtle import*
+
+# Objectifs:
+# Methode:
+# Besoins:
+# Connus:
+# Entrees:
+# Sorties:
+# Resultats:
+# Hypotheses:
