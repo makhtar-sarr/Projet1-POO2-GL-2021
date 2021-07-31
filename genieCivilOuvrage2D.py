b41cb477083df3e91c1f1753d04f6dcbd6836849
fatou-diouf <63673793+fatou-diouf@users.noreply.github.com>
2021-07-31 16:40:15 +0000

    Add files via upload

--- genieCivilOuvrage2D.py
+++ genieCivilOuvrage2D.py
@@ -1,7 +1,7 @@
 """Cet module contient des fonction pour dessiner
 des figure geometrique avec la bibiotheque Turtle.
 
-Auteurs: Fatou DIOUF, <votre mail ici>
+Auteurs: Fatou DIOUF, fatou.diouf2@univ-thies.sn
          Makhtar SARR, makhtar.sarr@univ-thies.sn.
 """
 
