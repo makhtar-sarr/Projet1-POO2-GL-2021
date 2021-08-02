368459cdbf1ec54fb9009568b0e6273f212632ab
fatou-diouf <63673793+fatou-diouf@users.noreply.github.com>
2021-08-02 10:25:20 +0000

    Add files via upload
    j'ai apporté une modification dans @genieCivilOuvrage2D.py à la fonction elypse.
    J'ai ajouté le pied du pont, pour qu'elle soit visible j'ai mis les lignes précédentes en commentaires.

--- pont.py
+++ pont.py
@@ -5,40 +5,67 @@ from turtle import*
 #tortue2 = Turtle()
 #tortue3 = Turtle()
 
-des.figure.penup()
-des.figure.setpos(-150, 0)
-des.figure.pendown()
-des.figure.begin_fill()
-des.rectangle(75, 20, "blue", "blue")
-des.figure.end_fill()
+#des.figure.penup()
+#des.figure.setpos(-150, 0)
+#des.figure.pendown()
+#des.figure.begin_fill()
+#des.rectangle(75, 20, "blue", "blue")
+#des.figure.end_fill()
 
-des.figure.penup()
-des.figure.setpos(-137, 20)
-des.figure.pendown()
-des.figure.begin_fill()
-des.rectangle(50, 20, "blue", "blue")
-des.figure.end_fill()
+#des.figure.penup()
+#des.figure.setpos(-137, 20)
+#des.figure.pendown()
+#des.figure.begin_fill()
+#des.rectangle(50, 20, "blue", "blue")
+#des.figure.end_fill()
 
-des.figure.penup()
-des.figure.setpos(-20, 0)
-des.figure.pendown()
-des.figure.begin_fill()
-des.rectangle(75, 20, "blue", "blue")
-des.figure.end_fill()
+#des.figure.penup()
+#des.figure.setpos(-20, 0)
+#des.figure.pendown()
+#des.figure.begin_fill()
+#des.rectangle(75, 20, "blue", "blue")
+#des.figure.end_fill()
 
-des.figure.penup()
-des.figure.setpos(-7, 20)
-des.figure.pendown()
-des.figure.begin_fill()
-des.rectangle(50, 20, "blue", "blue")
-des.figure.end_fill()
+#des.figure.penup()
+#des.figure.setpos(-7, 20)
+#des.figure.pendown()
+#des.figure.begin_fill()
+#des.rectangle(50, 20, "blue", "blue")
+#des.figure.end_fill()
 
-des.figure.penup()
-des.figure.setpos(-237, 40)
-des.figure.pendown()
-des.figure.width(2)
-des.figure.goto(145, 40)
+#des.figure.penup()
+#des.figure.setpos(-237, 40)
+#des.figure.pendown()
+#des.figure.width(2)
+#des.figure.goto(145, 40)
 
-for i in range(3):
-    des.demiCercle(64, "blue")
-    des.figure.left(90)
+for i in range(1):
+ #   des.demiCercle(64, "blue")
+  #  des.figure.left(90)
+
+#Pour la fin du pont, j'ai essayé cela
+
+    speed(5)
+    pensize(3)
+    color("dodgerblue3")
+
+    long = 217
+    larg = 32
+    ht()
+    begin_fill()
+    for i in range(2):
+        forward(long)
+        left(90)
+        forward(larg)
+        left(90)
+    up()
+    goto(38,larg)
+    pd()
+    long -= 76
+    larg -=3
+    for i in range(2):
+        forward(long) 
+        left(90)
+        forward(larg)
+        left(90)
+    end_fill
