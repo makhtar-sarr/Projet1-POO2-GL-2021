8758764b904058a1b9c5f4297b8d7f556f134ef8
makhtar-sarr <makhtar.sarr@univ-thies.sn>
2021-08-08 11:08:51 +0000

    my commit

--- genieCivilOuvrage2D.py
+++ genieCivilOuvrage2D.py
@@ -10,53 +10,53 @@ from math import*
 
 figure = Turtle()
 
-# Objectifs: Dessiner un cercle avec la bibliotheque Turtle.
-# Methode: Usage de la methode circle.
-# Besoins: rayon et la couleur (optionnelle)
+# Objectifs: Dessiner un cercle avec la bibliothèque Turtle.
+# Methode: Utilisation de la méthode circle.
+# Besoins: rayon et couleur (optionnelle).
 # Connus: -
-# Entrees: rayon, couleur (optionnelle)
+# Entrees: rayon.
 # Sorties: -
-# Resultats: Dessin d'un cercle
-# Hypotheses: rayon > 0
+# Resultats: dessin d'un cercle.
+# Hypotheses: rayon > 0.
 def cercle(rayon, couleur = "black"):
     figure.color(couleur)
     figure.circle(rayon)
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: Dessiner un demi-cercle avec la bibliothèque Turtle.
+# Methode: Utilisation de la méthode circle.
+# Besoins: rayon et couleur (optionnelle).
+# Connus: -
+# Entrees: rayon.
+# Sorties: -
+# Resultats: dessin d'un demi-cercle.
+# Hypotheses: rayon > 0.
 def demiCercle(rayon, couleur = "black"):
     figure.left(90)
     figure.color(couleur)
     figure.circle(rayon, 180)
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: Dessiner un carré avec la bibliothèque Turtle.
+# Methode: Utilisation des méthodes forward et left dans une boucle for.
+# Besoins: coté, couleur (optionnelle).
+# Connus: -
+# Entrees: coté.
+# Sorties: -
+# Resultats: dessin d'un carré.
+# Hypotheses: coté > 0.
 def carre(cote, couleur = "black", remplis = "white"):
     figure.color(couleur, remplis)
     for i in range(4):
         figure.forward(cote)
         figure.left(90)
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: Dessiner un triangle avec la bibliothèque Turtle.
+# Methode: Utilisation des méthodes de directions.
+# Besoins: coteA,coteB,coteC, couleurs (optionnelle).
+# Connus: -
+# Entrees: coteA,coteB,coteC.
+# Sorties: -
+# Resultats: dessin d'un triangle.
+# Hypotheses: coteA > 0,coteB > 0,coteC > 0.
 def triangle(coteA, coteB, coteC, couleur = "black", couleur1 = "white"):
     figure.color(couleur, couleur1)
     angleB = degrees(acos(((coteA**2) + (coteB**2) - (coteC**2))/(2*coteA*coteB)))
@@ -69,14 +69,14 @@ def triangle(coteA, coteB, coteC, couleur = "black", couleur1 = "white"):
     angleA = degrees(acos(((coteB**2) + (coteA**2) - (coteC**2))/(2*coteB*coteA)))
     figure.left(180 - angleA)
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: Dessiner un rectangle avec la bibliothèque Turtle.
+# Methode: utilisation des méthodes de direction dans une boucle for.
+# Besoins: longueur,largeur, couleur.
+# Connus: -
+# Entrees: longueur,largeur.
+# Sorties: -
+# Resultats: dessin d'un triangle.
+# Hypotheses: longueur > 0, largeur > 0, longueur > largeur.
 def rectangle(longueur, largeur, couleur = "black", couleur2='black'):
     figure.color(couleur, couleur2)
     if longueur != largeur:
@@ -88,14 +88,15 @@ def rectangle(longueur, largeur, couleur = "black", couleur2='black'):
     else:
         print("erreur")
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: Dessin d'un polygone avec la bibliothèque turtle.
+# Methode: Utilisation des méthodes de direction dans une boucle for avec des test de condition et l'utilisation.
+#  des méthodes triangle, carre et rectangle.
+# Besoins: nbrCote, cote1, cote2,(optionnel) cote3 (optionnel).
+# Connus: -
+# Entrees: nbrCote, cote1, cote2, cote3.
+# Sorties: -
+# Resultats: dessin d'un polygone.
+# Hypotheses: nbrCote >= 3, cote1 > 0, cote2 > 0, cote3 > 0.
 def polygone(nbrCote, cote1, cote2 = 0, cote3 = 0):
     if nbrCote == 3 and cote3 != 0:
         triangle(cote1, cote2, cote3)
@@ -110,14 +111,14 @@ def polygone(nbrCote, cote1, cote2 = 0, cote3 = 0):
     else:
         print("erreur")
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: Dessin d'un trapèze avec la bibliothèque turtle
+# Methode: Utilisation des méthodes de direction
+# Besoins: base1,base2,coteD, coteG, couleurs(optionnel)
+# Connus: -
+# Entrees: base1,base2,coteD, coteG
+# Sorties: -
+# Resultats: dessin d'un trapèze
+# Hypotheses: base1 > 0, base2 > 0, base1 > base2,coteD > 0, cote
 def trapeze(base1, base2, coteD, coteG, couleur = "black", couleur2 = "black"):
     figure.color(couleur, couleur2)
     figure.forward(base1)
@@ -128,15 +129,14 @@ def trapeze(base1, base2, coteD, coteG, couleur = "black", couleur2 = "black"):
     figure.left(150)
     figure.forward(coteG)
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
-
+# Objectifs: Dessiner un losange avec la bibliothèque turtle
+# Methode: utilisation des méthodes de direction dans une boucle for
+# Besoins: coté, couleur (optionnel)
+# Connus: -
+# Entrees: coté
+# Sorties: -
+# Resultats: Dessin d'un losange
+# Hypotheses: coté > 0
 def losange(cote, couleur = "black", couleur2 = "black"):
     figure.color(couleur, couleur2)
     figure.right(-30)
@@ -144,26 +144,23 @@ def losange(cote, couleur = "black", couleur2 = "black"):
         figure.forward(cote)
         figure.right(60*(1+i%2))
 
-# Objectifs:
-# Methode:
-# Besoins:
-# Connus:
-# Entrees:
-# Sorties:
-# Resultats:
-# Hypotheses:
+# Objectifs: dessiner un ellipse avec la bibliothèque turtle
+# Methode: utilisation des méthodes de direction dans une boucle for
+# Besoins: rayon , couleur (optionnel)
+# Connus: -
+# Entrees: rayon
+# Sorties: -
+# Resultats: dessin d'une ellipse
+# Hypotheses: rayon > 0
 def elypse(rayon, couleur = "black"):
     figure.color(couleur)
     figure.left(135)
     for i in range(2) :
         figure.circle(rayon, 90)
         figure.circle(rayon//4, 90)
+
 def demiElypse(rayon, couleur = "black"):
     figure.left(45)
     figure.circle(rayon//35, 90)
     figure.circle(rayon, 90)
     figure.circle(rayon//35, 90)
-
-elypse(100)
-
-exitonclick()
